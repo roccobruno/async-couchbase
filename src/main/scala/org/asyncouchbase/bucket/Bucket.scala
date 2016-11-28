ad754fccd83d740a7db1bb9f92eba90614ecfed0
Rocco Bruno <rocco.bruno@digital.hmrc.gov.uk>
2016-11-28 18:44:45 +0000

    Clean up

--- src/main/scala/org/asyncouchbase/bucket/Bucket.scala
+++ src/main/scala/org/asyncouchbase/bucket/Bucket.scala
@@ -4,11 +4,9 @@ import com.couchbase.client.java.document.JsonDocument
 import com.couchbase.client.java.document.json.JsonObject
 import com.couchbase.client.java.query.{AsyncN1qlQueryResult, AsyncN1qlQueryRow, N1qlQuery}
 import com.couchbase.client.java.{AsyncBucket, PersistTo, ReplicateTo}
-import org.asyncouchbase.Converters
 import org.asyncouchbase.Converters.observable2Enumerator
 import play.api.libs.iteratee.Iteratee
 import play.api.libs.json.{Json, Reads, Writes}
-import play.libs.concurrent.Futures
 import rx.lang.scala.JavaConversions.toScalaObservable
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -16,28 +14,21 @@ import scala.concurrent.Future
 
 trait Bucket {
 
+
+  def toFuture[T](observable: rx.Observable[T]) = toScalaObservable(observable).toBlocking.toFuture
+
   def asyncBucket: AsyncBucket
 
 
-  private[bucket] def gget[T](key: String)(bucket: AsyncBucket)(implicit r: Reads[T]): Future[Option[T]] = {
-
-
-
-    toScalaObservable(bucket.get(key)).toBlocking.toFuture map {
-      jsonDocument =>
-        Json.parse(jsonDocument.content().toString).asOpt[T]
-    }
-  }
-
-  def get[T](key: String)(implicit r: Reads[T]): Future[Option[T]] = gget[T](key)(asyncBucket)(r)
+  def get[T](key: String)(implicit r: Reads[T]): Future[Option[T]] = toFuture(asyncBucket.get(key)) map (doc => Json.parse(doc.content().toString).asOpt[T])
 
   def upsert[T](key: String, entity: T, persistTo: PersistTo = PersistTo.NONE, replicateTo: ReplicateTo = ReplicateTo.NONE)(implicit r: Writes[T]): Future[OpsResult] = {
     val ent = JsonObject.fromJson(r.writes(entity).toString())
-    toScalaObservable(asyncBucket.upsert(JsonDocument.create(key, ent))).toBlocking.toFuture map {
+    toFuture(asyncBucket.upsert(JsonDocument.create(key, ent))) map {
       _ =>
-        OpsResult(true,"")
+        OpsResult(isSuccess = true,"")
     } recover {
-      case ex: Throwable => OpsResult(false, ex.getMessage)
+      case ex: Throwable => OpsResult(isSuccess = false, ex.getMessage) //TODO catch the right exception and transform
     }
   }
 
@@ -48,20 +39,13 @@ trait Bucket {
       observable2Enumerator[AsyncN1qlQueryRow](row.rows()) run Iteratee.fold(List.empty[AsyncN1qlQueryRow]) { (l, e) => e :: l } map {
         _.reverse
       } map {
-        s =>
-          s map {
-            ss =>
-              r.reads(Json.parse(ss.value().toString)).get
-          }
+        s => s map(ss => r.reads(Json.parse(ss.value().toString)).get)
       }
     }
 
-    val toFuture: Future[AsyncN1qlQueryResult] = toScalaObservable(asyncBucket.query(query)).toBlocking.toFuture
-
     for {
-      obervable <- toFuture
-      results <- convert(obervable)
-
+      observable <- toFuture(asyncBucket.query(query))
+      results <- convert(observable)
     } yield results
 
   }
